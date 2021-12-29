4d04bd4b8c9ba4e6e4fc357a38d58b8ab317f0b4
Aamir <aprocks16@gmail.com>
2021-12-29 19:03:04 +0530

    Basic

---
+++ main.py
@@ -0,0 +1,7 @@
+import webbrowser
+import streamlit as st
+
+
+webbrowser.open('https://my.kis.in/')
+webbrowser.open('https://classroom.google.com/u/0/h')
+webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
