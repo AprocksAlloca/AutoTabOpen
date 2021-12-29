29027eda4aba0be987cdb3915fc34fe22d1bc064
Aamir <aprocks16@gmail.com>
2021-12-29 19:59:27 +0530

    Deploy #1

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import webbrowser
 import streamlit as st
 
+st.title("Open all your tabs in a click")
+st.write("Add all the links in the bellow text boxs and once added, click on 'Open Tabs'")
+button = st.button("Open Tabs")
 
-webbrowser.open('https://my.kis.in/')
-webbrowser.open('https://classroom.google.com/u/0/h')
-webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
+
+link_1 = st.text_input("Enter Link #1")
+link_2 = st.text_input('Enter Link #2')
+link_3 = st.text_input('Enter Link #3')
+link_4 = st.text_input('Enter Link #4')
+link_5 = st.text_input('Enter Link #5')
+
+
+if button == True:
+    webbrowser.open(link_1)
+    webbrowser.open(link_2)
+    webbrowser.open(link_3)
+    webbrowser.open(link_4)
+    webbrowser.open(link_5)
